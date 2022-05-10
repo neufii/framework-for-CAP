8c3b7275e4e7af25dc1dcd3985c9ca7a0f132c4f
Panida W <neufii.s@gmail.com>
2022-05-10 16:20:19 +0700

    add distance calculator

--- app/Modules/Repositories/SampleDistanceCalculator.php
+++ app/Modules/Repositories/SampleDistanceCalculator.php
@@ -21,24 +21,6 @@ class SampleDistanceCalculator implements DistanceCalculatorInterface {
         //     return null;
         // }
 
-        //add questions to file
-        // $temporaryDirectory = (new TemporaryDirectory())->create();
-        // $filename = $temporaryDirectory->path('questions.dat');
-        // $jsonData = json_encode($questions,JSON_FORCE_OBJECT);
-        // file_put_contents($filename,$jsonData);
-
-        // $processArray = [$distanceCalculatorScript->type, $distanceCalculatorScript->path, $filename];
-
-        // $process = new Process($processArray);
-        // $process->run();
-        // if (!$process->isSuccessful()) {
-        //     throw new ProcessFailedException($process);
-        //     $temporaryDirectory->delete();
-        // }
-
-        // $distanceMatrix = $process->getOutput();
-        // $temporaryDirectory->delete();
-
         $distanceMatrix = array();
         for($i = 0; $i < count($questions); $i++){
             for($j = 0; $j < count($questions); $j++){
@@ -50,6 +32,44 @@ class SampleDistanceCalculator implements DistanceCalculatorInterface {
     }
 
     private function calculateDistance(String $question1, String $question2){
-        return 0;
+        $arrQ1 = json_decode($question1, true);
+        $arrQ2 = json_decode($question2, true);
+
+        $filteredQ1 = array();
+
+        foreach($arrQ1 as $key => $value){
+            if($value['type'] == 'number'){
+                array_push($filteredQ1, $value['content']);
+            }
+            else if(
+                $value['type'] == 'plus_key' || 
+                $value['type'] == 'min_key' || 
+                $value['type'] == 'mul_key' || 
+                $value['type'] == 'div_key'
+            ){
+                array_push($filteredQ1, $value['type']);
+            }
+        }
+
+        $filteredQ2 = array();
+
+        foreach($arrQ2 as $key => $value){
+            if($value['type'] == 'number'){
+                array_push($filteredQ2, $value['content']);
+            }
+            else if(
+                $value['type'] == 'plus_key' || 
+                $value['type'] == 'min_key' || 
+                $value['type'] == 'mul_key' || 
+                $value['type'] == 'div_key'
+            ){
+                array_push($filteredQ2, $value['type']);
+            }
+        }
+
+        $distance = 1 - count(array_intersect($filteredQ1, $filteredQ2)) / count(array_unique(array_merge($filteredQ1, $filteredQ2)));
+
+        // var_dump($distance);
+        return $distance;
     }
 }
